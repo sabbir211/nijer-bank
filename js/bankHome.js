011d8f9415756d472769c91d30d0da04b12697c7
sabbir <sabbir21122@gmail.com>
2022-02-13 08:21:41 +0600

    balance functioneted

--- js/bankHome.js
+++ js/bankHome.js
@@ -3,43 +3,41 @@ document.getElementById('depositBtn').addEventListener('click', function () {
     let depositInput = document.getElementById('depositInput');
     const inputNumber=parseInt(depositInput.value);
     if (inputNumber>=0) {
-        
-        calculateBalance(depositAmount, depositInput);
-        // balance calculation is here
-        const balance = document.getElementById('balance')
-        const convertedBalance = parseInt(balance.innerText);
-        balance.innerText = convertedBalance + inputNumber
+        calculateBalance(depositAmount, depositInput,true);
     }
     else{
-        alert("Type Integer Number")
+        alert("Type positive Number")
     }
-    // clear input value
-    depositInput.value = "";
 })
 document.getElementById('withdrawBtn').addEventListener('click', () => {
     const withdrawAmount = document.getElementById('withdrawAmount')
     const withdrawInput = document.getElementById('withdrawInput')
+    const inputNumber=parseInt(withdrawInput.value)
     const balance = document.getElementById('balance')
     const convertedBalance = parseInt(balance.innerText);
-    const inputNumber=parseInt(withdrawInput.value)
     if (convertedBalance>=inputNumber&& inputNumber>=0) {
-        calculateBalance(withdrawAmount, withdrawInput)
-    // balance calculation is here
-    balance.innerText = convertedBalance - inputNumber
+        calculateBalance(withdrawAmount, withdrawInput,false)
    } 
    else{
        alert("Money has finished. First Deposit then Withdraw.Or type integer number")
-   }
-   // clear input value
-    withdrawInput.value = ""
-    
+    }
 })
 
-function calculateBalance(previous, inputted) {
+function calculateBalance(previous, inputted,isIncrease) {
     const previousNum = parseInt(previous.innerText);
     const inputtedNum = parseInt(inputted.value);
     const add = previousNum + inputtedNum;
-    previous.innerText = add
+    previous.innerText = add;
+    // balance update 
+    const balance = document.getElementById('balance')
+    const convertedBalance = parseInt(balance.innerText);
+   if (isIncrease) {
+    balance.innerText = convertedBalance + inputtedNum
+
+   } else {
+    balance.innerText = convertedBalance - inputtedNum     
+   }
+   inputted.value=""
 }
 
 
